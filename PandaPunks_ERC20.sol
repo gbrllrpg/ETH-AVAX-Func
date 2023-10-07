cbd801ad0cff87f0061327e3579e0ee1c4136f9e
gbrllrpg <113593175+gbrllrpg@users.noreply.github.com>
2023-10-08 00:45:15 +0800

    Update and rename ERC20.sol to PandaPunks_ERC20.sol

--- PandaPunks_ERC20.sol
+++ PandaPunks_ERC20.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PandaPunks is ERC20, Ownable {
+contract MyToken is ERC20, Ownable {
     uint8 private constant _decimals = 1;
     uint256 private constant _initialSupply = 200 * (10 ** uint256(_decimals));
 
@@ -31,3 +31,6 @@ contract PandaPunks is ERC20, Ownable {
         return super.transferFrom(sender, recipient, amount);
     }
 }
+
+//spender - 0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199
+//recipient - 0xdD2FD4581271e230360230F9337D5c0430Bf44C0
